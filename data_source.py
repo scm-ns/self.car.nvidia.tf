92dbb9f444252712bd4f94e42de2b85ef9827cf7
scm197 <scm197@rutgers.edu>
2016-12-29 21:54:32 -0500

    add support to train from udacity test set

--- data_source.py
+++ data_source.py
@@ -1,6 +1,6 @@
 # Provides the data to the training set
-
-
+import pandas as pd
+import cv2
 
 
 class data_source():
@@ -15,7 +15,7 @@ class data_source():
         # To load data from udacity 
         self.data_set_dir = "./data_set";
         self.df = pd.read_csv("./data_set/data.csv")
-        self.df = self.df[df.frame_id = 'center_camera']
+        self.df = self.df[df.frame_id == 'center_camera']
         
         self.xs = [self.data_set_dir + idx for idx in self.df.filename]
         self.ys = df.angle
@@ -25,7 +25,6 @@ class data_source():
         self._data_set = list(zip(self.xs , self.ys))
         random.shuffle(self._data_set)
         self.xs , self.ys = zip(*_data_set) 
-
 
         self.train_xs = xs[:int(len(xs) * 0.8)]
         self.train_ys = ys[:int(len(ys) * 0.8)]
@@ -38,5 +37,28 @@ class data_source():
 
     
     def load_train_batch(batch_size):
-        
+        x_out = []
+        y_out = []
+
+        for i in range(0 , batch_size):
+            img = cv2.imread(self.train_xs[(self._train_batch_ptr + i ) % self.num_train_img])
+            distorted = img 
+            x_out.append(cv2.resize(distorted , (200 , 66))/255.0)
+            y_out.append([self.train_ys[(self._train_batch_ptr + i) % self.num_train_img]])
+        self._train_batch_ptr += batch_size ; 
+        return x_out , y_out;
+
+           
+    def load_val_batch(batch_size):
+        x_out = []
+        y_out = []
+        for i in range(0 , batch_size):
+            x_out.append(cv2.resize(cv2.imread(val_xs[(self._val_batch_ptr + i) % self.num_val_img]) , (200,60)) / 255.0)
+            y_out.append([self.val_ys[(self._val_batch_ptr + i) % num_val_img]])
+        self._val_batch_ptr += batch_size ; 
+        return x_out , y_out 
+            
+    # Temporary solution. See how the test set for udacity is set up
+    def load_test_set():
+        return load_train_batch(100)
 
--- train.py
+++ train.py
@@ -1,20 +1,10 @@
 import tensorflow as tf
 import nvidia_model 
+import data_source
 
-
-train_x
-train_y
-
-val_x
-val_x
-
-test_x
-test_y
-
-
-
-def train_net(max_iter = 20 , batch_size = 100):
+def train_net(max_iter = 20 , batch_size = 50):
         model = nvidia_model()
+        ds = data_source()
         n_ter = 0 ;
         with tf.Session() as sess:
             tf.initialize_all_variables().run()
@@ -22,18 +12,17 @@ def train_net(max_iter = 20 , batch_size = 100):
 
             while(n_ter < max_iter):
                 for batch_idx in range(ds.total_samples / batch_size):
-                    xs , ys = # 
+                    xs , ys = ds.load_train_batch(batch_size)
 
                     # Train the model using hte optimizer setup in the nvidia_model.py file
                     sess.run(nvidia_model.optimizer , feed_dict = {model.input : xs , model.output : ys , model.keep_prob : 0.8})
 
                     if batch_idx % 10 == 0:
-                        xs , ys = # training
                         train_acc = model.accuracy.eval(session = sess , feed_dict = {model.input : xs , model.output : ys , model.keep_prob : 1 })
                         print("step : %d training acc : %g" %(batch_idx , train_acc))
 
                     if batch_idx % 15 == 0:
-                        xs_val , ys_val = # data form validation
+                        xs_val , ys_val = ds.load_val_batch(batch_size)
                         train_acc = model.accuracy.eval(session = sess , feed_dict = {model.input : xs_val , model.output : ys_val , model.keep_prob : 1 })
                         print("step : %d validation acc : %g" %(batch_idx , train_acc))
 
@@ -44,9 +33,10 @@ def train_net(max_iter = 20 , batch_size = 100):
                 checkpoint_filename = "model" + n_iter + ".ckpt"
                 checkpoint_path = os.path.join(LOGDIR , checkpoint_filename)
                 filename = saver.save(sess , checkpoint_path)
-                print("model saved at end of iter %d : @ : %g" , %(n_iter , filename)); 
+                print("model saved at end of iter %d : @ : %g" %(n_iter , filename)); 
 
                 n_ter += 1; 
 
             # Test the trained model, at the end of all iterations
+            xs_test , ys_test = ds.load_test_batch(); 
             test_acc = model.accuracy.eval(session = sess , feed_dict = {model.input : xs_test , model.output : ys_test , model.keep_prob : 1.0}) 
